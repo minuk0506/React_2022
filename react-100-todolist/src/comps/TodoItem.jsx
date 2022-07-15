cb46e25c1466fc936cf8ddd06c0a9a3e7c8a503f
minuk0506 <leeminuk9238@daum.net>
2022-07-15 22:17:17 +0900

    0715

--- react-100-todolist/src/comps/TodoItem.jsx
+++ react-100-todolist/src/comps/TodoItem.jsx
@@ -1,9 +1,9 @@
 const TodoItem = (props) => {
-  const { todo, todo_delete } = props;
-
+  const { todo, functions } = props;
+  const { todo_delete, todo_complete } = functions;
   return (
-    <tr>
-      <td>{todo.t_data}</td>
+    <tr className={todo.t_comp ? "line flag" : "flag"}>
+      <td onClick={() => todo_complete(todo.t_id)}>{todo.t_data}</td>
       <td onClick={() => todo_delete(todo.t_id)}>&times;</td>
     </tr>
   );
