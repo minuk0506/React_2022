f1e9cfeb8727a6c21fa7849c130fbe3a54d36751
minuk0506 <leeminuk9238@daum.net>
2022-07-21 17:55:12 +0900

    0721

---
+++ react-700-score/src/App.js
@@ -0,0 +1,21 @@
+import logo from "./logo.svg";
+import "./App.css";
+import "./w3css.css";
+import { ScoreContextProvider } from "./context/ScoreContextProvider.jsx";
+import StudentList from "./comps/StudentList.jsx";
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+      </header>
+      <ScoreContextProvider>
+        <div className="w3-container w3-margin">
+          <StudentList />
+        </div>
+      </ScoreContextProvider>
+    </div>
+  );
+}
+
+export default App;
